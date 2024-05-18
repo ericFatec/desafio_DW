c8f618304abc83c4e31dcb370b07b0dd103b1395
peaceFrog <eric.paula@fatec.sp.gov.br>
2024-05-18 12:16:14 -0300

    Sending docker-compose files

--- src/app.py
+++ src/app.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template
+from connect_db import connect
+from flask import Flask, render_template, request, jsonify
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, String, Integer, Date, select
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask("__name__")
+
+Base = declarative_base()
+
+class Agenda(Base):
+    __tablename__ = 'agendamentos'
+
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    sobrenome = Column(String)
+    email = Column(String)
+    telefone = Column(String)
+    data_visita = Column(Date)
+    destino = Column(String)
+
+Session = sessionmaker()
 
 @app.route('/')
 def home():
@@ -17,6 +36,55 @@ def destino():
 @app.route('/contact')
 def contact():
     return render_template('contato.html', current_page='contato')
+
+@app.route('/post', methods=['POST'])
+def send_db():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        sobrenome = request.form['sobrenome']
+        email = request.form['email']
+        telefone = request.form['telefone']
+        datavisita = request.form['data_visita']
+        destino=request.form['cardIdentifier']
+
+        # Connect to the database
+        database = connect()
+
+        # Bind the engine to the Base
+        Base.metadata.bind = database
+
+        # Bind the session to the engine
+        Session.configure(bind=database)
+
+        # Create a session instance
+        session = Session()
+
+        query = select(Agenda).where(Agenda.data_visita == datavisita, Agenda.destino == destino)
+        availability = session.execute(query).fetchone()
+
+        # Close the session
+        session.close()
+
+        if availability:
+            return jsonify({'message': 'Data indisponível!'})
+        
+        # Create a new session for adding data
+        Session.configure(bind=database)
+        session = Session()
+
+        # Create a new Cliente object
+        new_cliente = Agenda(nome=nome, sobrenome=sobrenome, email=email, telefone=telefone, data_visita=datavisita, destino=destino)
+
+        # Add the object to the session
+        session.add(new_cliente)
+
+        # Commit the session to save changes to the database
+        session.commit()
+
+        # Close the session
+        session.close()
+
+        return jsonify({'message': 'Dados enviados com sucesso!'})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
