92db615de2075a24fa4061a7fb8764ffb39cf56b
peaceFrog <eric.paula@fatec.sp.gov.br>
2024-05-20 19:53:44 -0300

    Trying Werkzeug Proxy for Nginx

--- src/app.py
+++ src/app.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template, request, jsonify, send_from_directory
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, String, Integer, Date, select
 from sqlalchemy.ext.declarative import declarative_base
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask("__name__", static_url_path='/static')
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 Base = declarative_base()
 
